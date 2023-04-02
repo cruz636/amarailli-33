1bd0d73db96cb2b23d452f179e6fcef89ed992cc
Juan Barneix <juanbarcruz96@gmail.com>
2023-04-01 22:38:06 -0300

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "amarailli-33",
+  url: "https://amarailli-33.botics.co",
+  api: "https://amarailli-33.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
